2e71e968a6c1fed4cadce8bff74c07b032f9186b
tokkiumai <kiwidancebad@gmail.com>
2023-11-02 19:08:00 +0300

    cleanup

--- pvol1/src/App.tsx
+++ pvol1/src/App.tsx
@@ -1,8 +1,5 @@
 import { createUseStyles, ThemeProvider } from "react-jss";
 
-import RootHeader from "./components/RootHeader";
-import RootWorkspace from "./components/RootWorkspace";
-
 import theme from './theme'
 import './App.css'
 
@@ -10,12 +7,6 @@ function App() {
   let classes = useStyles()
   return (
     <ThemeProvider theme={theme}>
-      <header className={classes.header}>
-        <RootHeader/>
-      </header>
-      <main className={classes.main}>
-        <RootWorkspace/>
-      </main>
     </ThemeProvider>
   )
 }
